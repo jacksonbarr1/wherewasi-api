88df21fb403d09932c799a065c9b5238b3e6cbdc
Jackson Barr <jacksonbarr2021@gmail.com>
2025-07-28 12:49:37 -0400

    Create test environment

---
+++ src/test/java/com/wherewasi/wherewasiapi/AbstractIT.java
@@ -0,0 +1,36 @@
+package com.wherewasi.wherewasiapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Testcontainers
+public abstract class AbstractIT {
+
+    @Container
+    static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo:latest")
+            .withExposedPorts(27017);
+
+    @Container
+    static GenericContainer<?> redisContainer = new GenericContainer<>("redis:latest")
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        mongoDbContainer.start();
+        registry.add("spring.data.mongodb.host", mongoDbContainer::getHost);
+        registry.add("spring.data.mongodb.port", () -> mongoDbContainer.getFirstMappedPort());
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
+        registry.add("jwt.secret", () -> "testsecret");
+    }
+
+
+}
---
+++ src/test/java/com/wherewasi/wherewasiapi/repository/UserRepositoryIT.java
@@ -0,0 +1,44 @@
+package com.wherewasi.wherewasiapi.repository;
+
+import com.wherewasi.wherewasiapi.AbstractIT;
+import com.wherewasi.wherewasiapi.enumeration.UserRole;
+import com.wherewasi.wherewasiapi.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserRepositoryIT extends AbstractIT {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void shouldSaveAndFindUserByEmail() {
+        User user = User.builder()
+                .name("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .password("password")
+                .role(UserRole.ROLE_USER)
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        assertNotNull(savedUser);
+        assertNotNull(savedUser.getId());
+
+        Optional<User> foundUserOptional = userRepository.findByEmail(user.getEmail());
+
+        assertThat(foundUserOptional).isPresent();
+        User foundUser = foundUserOptional.get();
+        assertThat(foundUser.getId()).isEqualTo(savedUser.getId());
+        assertThat(foundUser.getName()).isEqualTo("John");
+        assertThat(foundUser.getLastName()).isEqualTo("Doe");
+        assertThat(foundUser.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(foundUser.getRole()).isEqualTo(UserRole.ROLE_USER);
+    }
+
+}
