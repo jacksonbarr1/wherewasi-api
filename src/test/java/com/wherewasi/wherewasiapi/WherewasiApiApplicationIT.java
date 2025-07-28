1ea002d3550b03f24c40dffc6045f6b973ee4ae8
Jackson Barr <jacksonbarr2021@gmail.com>
2025-07-28 13:01:02 -0400

    Rename contextLoads IT class to fit pattern

--- src/test/java/com/wherewasi/wherewasiapi/WherewasiApiApplicationIT.java
+++ src/test/java/com/wherewasi/wherewasiapi/WherewasiApiApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class WherewasiApiApplicationTests extends AbstractIT {
+class WherewasiApiApplicationIT extends AbstractIT {
 
 	@Test
 	void contextLoads() {
