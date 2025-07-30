55ca46edd568a04b40ddbe930c9a1218d853c8ba
Jackson Barr <jacksonbarr2021@gmail.com>
2025-07-30 15:30:04 -0400

    Add validation to query parameter in /search

--- src/main/java/com/wherewasi/wherewasiapi/controller/ShowController.java
+++ src/main/java/com/wherewasi/wherewasiapi/controller/ShowController.java
@@ -2,10 +2,13 @@ package com.wherewasi.wherewasiapi.controller;
 
 import com.wherewasi.wherewasiapi.dto.response.ShowMetadataDTO;
 import com.wherewasi.wherewasiapi.service.ShowService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +22,7 @@ public class ShowController {
     private final ShowService showService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<ShowMetadataDTO>> searchShows(String query) {
+    public ResponseEntity<List<ShowMetadataDTO>> searchShows(@RequestParam @NotBlank @Size(min = 2, max = 50) String query) {
         List<ShowMetadataDTO> shows = showService.searchShows(query);
         return ResponseEntity.ok(shows);
     }
