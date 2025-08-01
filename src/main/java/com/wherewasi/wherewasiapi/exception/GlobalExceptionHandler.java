f91d799e1f28ca44525dd5f5cea93164ad0cc519
Jackson Barr <jacksonbarr2021@gmail.com>
2025-08-01 12:03:53 -0400

    Add global exception handler for request validation exceptions

--- src/main/java/com/wherewasi/wherewasiapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wherewasi/wherewasiapi/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.wherewasi.wherewasiapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,5 +21,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(WeakPasswordException.class)
     public ResponseEntity<String> handleWeakPasswordException(WeakPasswordException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getBindingResult().getFieldError().getDefaultMessage());
     }
 }
