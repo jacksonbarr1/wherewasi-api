df15c7c9839fba71f570e4b357db8babe6f82eaa
Jackson Barr <jacksonbarr2021@gmail.com>
2025-08-01 12:18:48 -0400

    Populate ShowDetailsDTO

--- src/main/java/com/wherewasi/wherewasiapi/dto/response/ShowDetailsDTO.java
+++ src/main/java/com/wherewasi/wherewasiapi/dto/response/ShowDetailsDTO.java
@@ -1,9 +1,38 @@
 package com.wherewasi.wherewasiapi.dto.response;
 
+import com.wherewasi.wherewasiapi.model.Show;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * DTO for show-specific page after user selects a preview from the homepage or search results.
  * Does not contain episode specific information, only data needed to display the show.
  */
 
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShowDetailsDTO {
+    private Integer id;
+    private String homepage;
+    private String name;
+    private String overview;
+    private String firstAirDate;
+    private String posterPath;
+    private String backdropPath;
+    private Float popularity;
+    private Float voteAverage;
+    private Integer voteCount;
+    private Integer numberOfSeasons;
+    private Map<Integer, Integer> seasonEpisodeCounts;
+
+    private List<Show.Creator> creators;
+    private List<Show.Genre> genres;
+    private List<Show.Network> networks;
 }
